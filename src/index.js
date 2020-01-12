832b802a07593e416822acdb271fb2d484750d9c
t-kurihara <takuya.kurihara00@gmail.com>
2020-01-12 23:22:51 +0900

    [add] graphql provicer.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient, { gql } from 'apollo-boost';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({ url: 'http://localhost:4000/graphql' });
+
+const query = gql`
+  {
+    totalUsers
+    totalPhotos
+  }
+`;
+
+client
+  .query({ query })
+  .then(({ data }) => console.log('data', data))
+  .catch(console.error);
+
+ReactDOM.render(
+  <ApolloProvider>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
